765d59a105d51adccb4915cd46ba213779f5603f
coder <sowpharell98@hotmail.com>
2023-07-03 13:18:49 +0000

    teste2

--- app/Http/Controllers/PanierController.php
+++ app/Http/Controllers/PanierController.php
@@ -5,38 +5,33 @@ namespace App\Http\Controllers;
 use App\Models\Commande;
 use Illuminate\Http\Request;
 use App\Cart;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 use Stripe\Charge;
 use Stripe\Stripe;
-
 class PanierController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth');
     }
-
-    public function index()
-    {
-        if (!Session::has('panier')) {
+    public function index(){
+        if(!Session::has('panier')){
             return view('client.cart');
         }
 
-        $oldCart = Session::has('panier') ? Session::get('panier') : null;
+        $oldCart = Session::has('panier')? Session::get('panier'):null;
         $cart = new Cart($oldCart);
         return view('client.cart', ['produits' => $cart->items]);
     }
-
-    public function show($id)
-    {
-        $oldCart = Session::has('panier') ? Session::get('panier') : null;
+    public function show($id){
+        $oldCart = Session::has('panier')? Session::get('panier'):null;
         $cart = new Cart($oldCart);
         $cart->removeItem($id);
 
-        if (count($cart->items) > 0) {
+        if(count($cart->items) > 0){
             Session::put('panier', $cart);
-        } else {
+        }
+        else{
             Session::forget('panier');
         }
 
@@ -44,10 +39,9 @@ class PanierController extends Controller
         return redirect('/paniers');
     }
 
-    public function update(Request $request, $id)
-    {
-        //print('the product id is '.$request->id.' And the product qty is '.$request->quantity);
-        $oldCart = Session::has('panier') ? Session::get('panier') : null;
+    public  function update(Request $request, $id){
+           //print('the product id is '.$request->id.' And the product qty is '.$request->quantity);
+        $oldCart = Session::has('panier')? Session::get('panier'):null;
         $cart = new Cart($oldCart);
         $cart->updateQty($id, $request->quantity);
         Session::put('panier', $cart);
@@ -56,26 +50,23 @@ class PanierController extends Controller
         return redirect('paniers');
     }
 
-    public function paiement(Request $request)
-    {
+    public function paiement(Request $request){
         $prixLivraison = $request->input('prix');
         $lieuLivraison = $request->input('lieuLivraison');
         $prixAchat = $request->input('prixAchat');
-        $total = $prixLivraison + $prixAchat;
+        $total = $prixLivraison +  $prixAchat;
         Session::put('prixLivraison', $prixLivraison);
         Session::put('lieuLivraison', $lieuLivraison);
         Session::put('total', $total);
 
-        return view('client.checkout', ['prixLivraison' => $prixLivraison, 'total' => $total, 'lieuLivraison' => $lieuLivraison]);
+        return view('client.checkout',['prixLivraison'=>$prixLivraison,'total'=>$total,'lieuLivraison'=>$lieuLivraison]);
     }
 
-    public function payer(Request $request)
-    {
-        $user = Auth::user();
+    public function payer(Request $request){
         Stripe::setApiKey('sk_test_51NK32UDJMpV7WgnsYMifkyE3rEMFeAnHBtelw0UTbRnNf8B7LZcjoRT1inTNdihkvhWOzgvxnU9mWKp45reuJdPK00NIkycWo8');
-        try {
+        try{
             $cart = Session::has('total') ? Session::get('total') : null;
-            $panier = Session::has('panier') ? Session::get('panier') : null;
+            $panier = Session::has('panier')? Session::get('panier'):null;
             $charge = Charge::create(array(
                 "amount" => $cart * 100,
                 "currency" => "usd",
@@ -94,18 +85,8 @@ class PanierController extends Controller
             $commande->users_id = $request->input('idUser');
 
             $commande->save();
-            $commandes = Commande::where('payement_id', $charge->id)->first();
 
-            $commandes->transform(function ($commande, $key) {
-                $commande->panier = unserialize($commande->panier);
-                return $commande;
-
-                $email = $user->email;
-               // dd($email);
-
-            });
-
-        } catch (\Exception $e) {
+        } catch(\Exception $e){
             Session::put('error', $e->getMessage());
             return redirect()->route('paniers.paiement')->with('error', $e->getMessage());
         }
